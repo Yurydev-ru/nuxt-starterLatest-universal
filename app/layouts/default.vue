c851796aa00ece0158a334b59d6e4370862a1592
Yuryd <yurydevru@gmail.com>
2025-09-24 01:50:21 +0200

    feat: add initial project files including LICENSE, SCSS, and Vue components

---
+++ app/layouts/default.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <AppHeader />
+  <main class="app-main">
+    <slot />
+  </main>
+    <AppFooter />
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+    definePageMeta({
+        layout: 'default',
+        title: 'Home Page'
+    })
+</script>
+
+<template>
+    <p class="text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam est reprehenderit magnam, quo accusantium delectus distinctio magni? Cumque, odit optio!</p>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
